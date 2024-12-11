388e3d757d0a8e8e2b4b0de6c91de55de2f1df53
Tetiana Deineka <tetianadeinekaa@gmail.com>
2024-12-11 13:47:30 +0200

    added clicker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Character from './components/Character.jsx';
 import { Route, Routes } from 'react-router-dom';
 import { StartPage } from './components/StartPage/StartPage.jsx';
+import Clicker from './components/Clicker/Clicker.jsx';
 
 export const App = () => {
   return (
     <Routes>
       <Route index element={<StartPage />} />
-      <Route path="game" element={<Character />} />
+      <Route path="game" element={<Clicker />} />
     </Routes>
   );
 }
---
+++ src/components/Clicker/Clicker.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import coin from "../../assets/images/coin.png";
+import "./Clicker.css";
+
+const Clicker = () => {
+  const [coins, setCoins] = useState(0);
+  const [effects, setEffects] = useState([]);
+
+  const addCoins = (amount, x, y) => {
+    setCoins((prevCoins) => prevCoins + amount);
+
+    const id = Date.now();
+    setEffects((prevEffects) => [...prevEffects, { id, x, y }]);
+
+    setTimeout(() => {
+      setEffects((prevEffects) =>
+        prevEffects.filter((effect) => effect.id !== id)
+      );
+    }, 1000);
+  };
+
+  const handleClick = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    addCoins(1, x, y); // Add 1 coin per click
+  };
+
+  return (
+    <div className="clickerWrap">
+      <div className="coins">
+        <img src={coin} alt="Coin" className="imgCoin" />
+        <span className="coinQuant">{coins}</span>
+      </div>
+
+      <div onClick={handleClick} className="clickerItem">
+        {Clicker}
+      </div>
+
+      {effects.map((effect) => (
+        <span
+          key={effect.id}
+          className="floatingText"
+          style={{ left: `${effect.x}px`, top: `${effect.y}px` }}
+        >+1</span>
+      ))}
+    </div>
+  );
+};
+
+export default Clicker;
+
