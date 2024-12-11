5d5726efa25a94eee2ed6ce4f8fb55180d524855
Tetiana Deineka <tetianadeinekaa@gmail.com>
2024-12-11 14:00:38 +0200

    full screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { StartPage } from './components/StartPage/StartPage.jsx';
 import Clicker from './components/Clicker/Clicker.jsx';
 
 export const App = () => {
+  useEffect(() => {
+    if (window.Telegram.WebApp) {
+      const webApp = window.Telegram.WebApp;
+
+      // Ініціалізація веб-програми
+      webApp.ready();
+
+      // Увімкнути повноекранний режим
+      webApp.expand();
+    }
+  }, []);
+
   return (
     <Routes>
       <Route index element={<StartPage />} />
